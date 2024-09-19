ced0002c1fa1a6a287ae6ddde203e8928ad920db
Shailenkaushik <107068730+Shailenkaushik@users.noreply.github.com>
2024-09-19 19:41:59 +0530

    connected signup page to db

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Header} from '../components/Header.jsx'
 import {SubHeading} from '../components/SubHeading.jsx'
 import {InputBox} from '../components/InputBox.jsx'
 import {Button} from '../components/Button.jsx'
 import { BottomWarning } from '../components/BottomWarning.jsx'
+import axios from 'axios';
+// import {axios} from 'axios'
  export const  Signup=()=> {
+  
+   const [firstName,setFirstName]=useState("");
+   const [lastName,setLastName]=useState("");
+   const [username,setUserName]=useState("");
+   const [password,setPassword]=useState("");
+   
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
    
@@ -12,10 +20,27 @@ import { BottomWarning } from '../components/BottomWarning.jsx'
       <div className='rounded-lg bg-white w-80 text-center p-2 h-max px-4'>
        <Header label={"Sign Up"} />
        <SubHeading label={"Enter your information to create an account"} ></SubHeading>
-       <InputBox label={"FirstName"} placeholder={"john"}/ >
-       <InputBox label={"LastName"} placeholder={"Doe"}/ >
-       <InputBox label={"Email"} placeholder={"johndoe@example.com"}/ >
-       <Button label={"signup"} />
+       <InputBox label={"FirstName"} onChange={e=>{
+        setFirstName(e.target.value);
+       }} placeholder={"john"}/ >
+       <InputBox label={"LastName"} onChange={e=>{
+        setLastName(e.target.value);
+       }}  placeholder={"Doe"}/ >
+       <InputBox label={"Email"} onChange={e=>{
+        setUserName(e.target.value);
+       }}  placeholder={"johndoe@example.com"}/ >
+       <InputBox label={"Password"} onChange={e=>{
+        setPassword(e.target.value);
+       }}  placeholder={"***"}/ >
+       <Button label={"signup"} onClick={()=>{
+        axios.post('http://localhost:3000/api/v1/user/signup',{
+          username,
+          firstName,
+          lastName,
+          password
+        });
+        console.log("post successful");
+       }} />
        
        <BottomWarning label={"Already have an account?"} buttonText={"login"} to={"/signin"} ></BottomWarning>
        
