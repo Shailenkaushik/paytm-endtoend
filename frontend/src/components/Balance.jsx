2ef185e12c3761ac1238b7cf4a4cb08315084b54
Shailenkaushik <107068730+Shailenkaushik@users.noreply.github.com>
2024-11-16 23:09:23 +0530

    Update Balance.jsx

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -4,7 +4,7 @@ export const  Balance=({balance}) =>{
   return (
     <div className='flex justify-start p-2'>
     <div className='font-medium '>
-      Your Balance  is quite good and go f sidjidsor it boy. 
+      Your Balance  is quite goosfdsdsdsdddsddd and go f sidjidsor it boy. 
     </div>
     <div className='font-light ml-4'>
         Rs {balance}
